ee04de577987036c2effd174039b0a1485905604
TerryXhx <TerryXhx@outlook.com>
2022-05-11 09:03:45 +0800

    Fix bug in libwasm.

--- sdk/wasm/sgx_wasm.cpp
+++ sdk/wasm/sgx_wasm.cpp
@@ -1,3 +1,4 @@
+#include "arch.h"
 #include "sgx_wasm.h"
 
 const uint8_t __attribute__((section(SGX_WASM_SEC_NAME))) sgx_wasm_sec_buf[SGX_WASM_SEC_SIZE] __attribute__((aligned (SE_PAGE_SIZE))) = {};
@@ -10,7 +11,7 @@ uint64_t sgx_wasm_get_size()
     return wasm_hdr->size;
 }
 
-uint8_t* get_sgx_wasm_sec_buf_addr()
+uint8_t* sgx_get_wasm_sec_buf_addr()
 {
     return reinterpret_cast<uint8_t*>(reinterpret_cast<uint64_t>(sgx_wasm_sec_buf));
 }
