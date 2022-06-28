38e3e5e467478919acb0e59a8d0e95a0334839c3
TerryXhx <TerryXhx@outlook.com>
2022-06-28 11:39:39 +0800

    Add wasm vm mr section.

--- common/inc/sgx_wasm.h
+++ common/inc/sgx_wasm.h
@@ -11,6 +11,11 @@ extern "C" {
 #define SGX_WASM_SEC_SIZE 20480
 // must be a multiple of 4096 (page size)
 
+#define SGX_WASM_VM_MR_SEC_NAME ".sgx_wasm_vm_mr"
+#define SGX_WASM_VM_MR_SEC_SIZE 4096
+// must be a multiple of 4096 (page size)
+// 4096 is large enough to store the measurement
+
 
 typedef struct _sgx_wasm_t {
     uint64_t size;
@@ -18,6 +23,8 @@ typedef struct _sgx_wasm_t {
 } sgx_wasm_t;
 
 uint8_t* sgx_get_wasm_sec_buf_addr();
+
+uint8_t* sgx_get_wasm_vm_mr_sec_buf_addr();
 
 #ifdef __cplusplus
 }
--- sdk/wasm/sgx_wasm.cpp
+++ sdk/wasm/sgx_wasm.cpp
@@ -2,8 +2,14 @@
 #include "sgx_wasm.h"
 
 const uint8_t __attribute__((section(SGX_WASM_SEC_NAME))) sgx_wasm_sec_buf[SGX_WASM_SEC_SIZE] __attribute__((aligned (SE_PAGE_SIZE))) = {};
+const uint8_t __attribute__((section(SGX_WASM_VM_MR_SEC_NAME))) sgx_wasm_vm_buf_mr[SGX_WASM_VM_MR_SEC_SIZE] __attribute__((aligned (SE_PAGE_SIZE))) = {};
 
 uint8_t* sgx_get_wasm_sec_buf_addr()
 {
     return reinterpret_cast<uint8_t*>(reinterpret_cast<uint64_t>(sgx_wasm_sec_buf));
+}
+
+uint8_t* sgx_get_wasm_vm_mr_sec_buf_addr()
+{
+    return reinterpret_cast<uint8_t*>(reinterpret_cast<uint64_t>(sgx_wasm_vm_buf_mr));
 }
